fd586754bc6ddb07bedd4d82a0d5b02ceb38fae8
unknown <codename100.alan@gmail.com>
2018-09-03 08:51:50 -0400

    Added navigation component for every page

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import Error from './components/Error';
+import Navigation from './components/Navigation';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route path='/' component={Home} exact />
-          <Route path='/about' component={About} />
-          <Route path='/contact' component={Contact} />
-          <Route path='/contact' component={Error} />
-        </Switch>
+        <div>
+          <Navigation />
+          <Switch>
+            <Route path='/' component={Home} exact />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/contact' component={Error} />
+          </Switch>
+        </div>
       </BrowserRouter>
     );
   }
