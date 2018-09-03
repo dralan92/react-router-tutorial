badd9b856cdc7a8ff0a53771fee1966a64947ef7
unknown <codename100.alan@gmail.com>
2018-09-03 07:56:29 -0400

    Added 2 more Routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
 import Home from './components/Home';
-
+import About from './components/About';
+import Contact from './components/Contact';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path='/' component={Home} />
+        <div>
+          <Route path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+        </div>
       </BrowserRouter>
     );
   }
