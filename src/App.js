d25c47d10dccf0a1eca97015263456e4ebe97c1a
unknown <codename100.alan@gmail.com>
2018-09-03 07:37:44 -0400

    added a route and component and compnent is showing for the route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
 
+
+const NewRoute =  () => {
+  return (
+    <div>
+      <p> New Route Component </p>
+    </div>
+  )
+}
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <BrowserRouter>
+        <Route path='/new' component={NewRoute} />
+      </BrowserRouter>
     );
   }
 }
