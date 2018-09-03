ab765b275cbf784a7ac33e5ea4f86fa56b3e5038
unknown <codename100.alan@gmail.com>
2018-09-03 07:48:39 -0400

    Added a Home component for the home route'/'

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
-
-
-const NewRoute =  () => {
-  return (
-    <div>
-      <p> New Route Component </p>
-    </div>
-  )
-}
+import Home from './components/Home';
 
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path='/new' component={NewRoute} />
+        <Route path='/' component={Home} />
       </BrowserRouter>
     );
   }
